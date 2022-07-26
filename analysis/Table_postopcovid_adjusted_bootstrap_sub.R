66a4bae1f6772bc3ce5185aae795bc5954bbe8c0
Colin Crooks (staff) <Colin.Crooks@nottingham.ac.uk>
2022-07-26 18:25:43 +0100

    Bootstrap and continuous post op risk
    Try bootstrap again with more coarse parallelisation
    Try plotting continuous daily risk

--- analysis/Table_postopcovid_adjusted_bootstrap_sub.R
+++ analysis/Table_postopcovid_adjusted_bootstrap_sub.R
@@ -8,20 +8,32 @@ set.seed(23097859)
 ###########################################################
 
 dt.tv <- data.table::setDT(arrow::read_feather(here::here("output","cohort_long.feather")))
-procedures <- c('Abdominal','Cardiac','Obstetrics','Orthopaedic','Thoracic', 'Vascular')
-
-
-covariates <- c(procedures,'sex','age.cat','bmi.cat','imd5','wave',
-                'vaccination.status.factor','Current.Cancer','Emergency','LOS.bin','Charl12','recentCOVID','previousCOVID','region')
-
-data.table::setkey(dt.tv,patient_id,tstart,tstop)
+procedures.sub <- c('Colectomy','Cholecystectomy',
+                    'HipReplacement','KneeReplacement')
+covariates <- c(procedures.sub,'sex','age.cat','bmi.cat','imd5','wave','LOS.bin',
+                'vaccination.status.factor','Current.Cancer','Emergency','Charl12','recentCOVID','previousCOVID','region')
 
 drop.vars <- names(dt.tv)[!(names(dt.tv) %in% c(covariates, 'patient_id', 'tstart','tstop','start','end','event','postop.covid.cohort','end.fu'))]
 
 dt.tv[,(drop.vars) := NULL]
-dt.tv <- dt.tv[(postop.covid.cohort)]
+
+
+dt.tv[, sub.op := (is.finite(Colectomy) & Colectomy ==T) |
+        (is.finite(Cholecystectomy) & Cholecystectomy == T) |
+        (is.finite(HipReplacement)  & HipReplacement == T) | 
+        (is.finite(KneeReplacement) & KneeReplacement == T)]
+
+data.table::setkey(dt.tv,patient_id,tstart,tstop)
+max.grp.col_(dt = 'dt.tv',
+             max.var.name = 'sub.op',
+             aggregate.cols = 'sub.op',
+             id.vars = c("patient_id","end.fu"))
+
+dt.tv[,(procedures.sub) := lapply(.SD,function(x) x==1), .SDcols = (procedures.sub)]
+dt.tv <- dt.tv[(postop.covid.cohort) & sub.op == T]
 gc()
-n.type.events <- sort(unique(dt.tv[(postop.covid.cohort) ,event]))[-1]
+
+n.type.events <- sort(unique(dt.tv[(postop.covid.cohort) & sub.op == T ,event]))[-1]
 
 library(doParallel)
 ncores <- parallel::detectCores(logical = F)
@@ -29,7 +41,7 @@ cl <- parallel::makeCluster(ncores)
 doParallel::registerDoParallel(cl)
 
 
-samples <-  foreach::foreach(
+samples.sub <-  foreach::foreach(
   i = 1:1000,
   .combine = cbind,
   .multicombine = T,
@@ -39,18 +51,17 @@ samples <-  foreach::foreach(
   .export = c(
     'n.type.events',
     'dt.tv',
-    'procedures',
+    'procedures.sub',
     'covariates'
   )
 ) %dopar% {
   dt.tv <-
     dt.tv[patient_id %in% sample(unique(patient_id), replace = T) &
             (postop.covid.cohort)]
-  post.op.covid.model <-
+  post.op.covid.model.sub <-
     lapply(n.type.events, function(i)
       survival::coxph(
-        survival::Surv(start, end, event == i) ~ Abdominal + Cardiac +
-          Obstetrics + Thoracic + Vascular +
+        survival::Surv(start, end, event == i) ~ Colectomy + Cholecystectomy  + KneeReplacement +
           age.cat + sex + bmi.cat + imd5 +
           vaccination.status.factor + region + Current.Cancer +
           Emergency + LOS.bin + wave + Charl12 + recentCOVID + previousCOVID,
@@ -74,16 +85,10 @@ samples <-  foreach::foreach(
         'end' = rep(30, newdata.rows),
         'event' = rep(F, newdata.rows),
         'patient_id' = 1:newdata.rows,
-        'Abdominal' = rep(T, newdata.rows),
-        'Cardiac' = rep(F, newdata.rows),
-        'Obstetrics' =
-          rep(F, newdata.rows),
-        'Orthopaedic' =
-          rep(F, newdata.rows),
-        'Thoracic' =
-          rep(F, newdata.rows),
-        'Vascular' =
-          rep(F, newdata.rows),
+        'Colectomy' = c(rep(T,newdata.rows)),
+        'Cholecystectomy'=c(rep(F,newdata.rows)),
+        'HipReplacement'=c(rep(F,newdata.rows)),
+        'KneeReplacement'=c(rep(F,newdata.rows)),
         'age.cat' = rep('(50,70]', newdata.rows),
         'sex' = rep('F', newdata.rows),
         'bmi.cat' = rep(levels(dt.tv$bmi.cat)[2], newdata.rows),
@@ -98,9 +103,9 @@ samples <-  foreach::foreach(
         'recentCOVID' = rep(F, newdata.rows),
         'previousCOVID' = rep(F, newdata.rows)
       )
-    if (predi <= length(procedures)) {
-      newdata.pred[, (procedures) := F]
-      newdata.pred[, (procedures[predi]) := c(F, T)]
+    if (predi <= length(procedures.sub)) {
+      newdata.pred[, (procedures.sub) := F]
+      newdata.pred[, (procedures.sub[predi]) := c(F, T)]
     } else {
       if (is.factor(dt.tv[!is.na(get(covariates[predi])), get(covariates[predi])])) {
         newdata.pred[, (covariates[predi]) :=  as.character(sort(unique(dt.tv[!is.na(get(covariates[predi])), get(covariates[predi])], na.rm = T)))]
@@ -116,7 +121,7 @@ samples <-  foreach::foreach(
     cuminc.cox(
       n.type.events = n.type.events,
       dt = 'dt.tv',
-      model = 'post.op.covid.model',
+      model = 'post.op.covid.model.sub',
       newdata = 'newdata.pred',
       day = 30
     )
@@ -124,10 +129,10 @@ samples <-  foreach::foreach(
   })
 }
 
-t.samples <- t(apply(samples,1,quantile,c(0.25,0.5,0.75)))
-boot.IQR <-apply(t.samples,1,function(x) paste0(x[2],' (',x[1],',',x[3],')'))
+t.samples <- t(apply(samples.sub,1,quantile,c(0.25,0.5,0.75)))
+boot.IQR.sub <-apply(t.samples,1,function(x) paste0(x[2],' (',x[1],',',x[3],')'))
 
 
-save(samples, file = here::here("output","postopcovid_adjusted_bootstrappedIQR.RData"))
+save(samples.sub, file = here::here("output","postopcovid_adjusted_bootstrappedIQR_sub.RData"))
 
-data.table::fwrite(boot.IQR, file = here::here("output","postopcovid_adjusted_bootstrappedIQR.csv"))
+data.table::fwrite(boot.IQR.sub, file = here::here("output","postopcovid_adjusted_bootstrappedIQR_sub.csv"))
---
+++ analysis/Table_postopcovid_tv_continuous_sub.R
@@ -0,0 +1,209 @@
+library(foreach)
+library(data.table)
+ncores <- parallel::detectCores(logical = T)
+data.table::setDTthreads(ncores)
+
+source(here::here("analysis","Utils.R"))
+
+###########################################################
+
+dt.tv.splits <- data.table::setDT(arrow::read_feather(here::here("output","cohort_postdisch_week_splits.feather")))
+dt.tv.splits <- dt.tv.splits[,.(patient_id,Colectomy,Cholecystectomy,HipReplacement,KneeReplacement,age.cat,sex,bmi.cat,imd5,wave,vaccination.status.factor,region,Current.Cancer,Emergency,
+                                week.post.disch,week.post.op,LOS.bin,Charl12,recentCOVID,previousCOVID,postcovid,
+                                tstart,tstop,end.fu,start,end,event,postop.covid.cohort,los.end,event.VTE,postcovid.VTE.cohort,study.start)]
+procedures <- c('Colectomy','Cholecystectomy',
+                'HipReplacement','KneeReplacement')
+
+# Start = 0 = day of operation
+dt.tv.splits[, `:=`(start = tstart - study.start,
+                    end = tstop - study.start)]
+dt.tv.splits <- dt.tv.splits[start >= 0,] # Need to start follow up on day after operation as can't identify order when events on same day
+
+dt.tv.splits[, sub.op := (is.finite(Colectomy) & Colectomy ==T) |
+               (is.finite(Cholecystectomy) & Cholecystectomy == T) |
+               (is.finite(HipReplacement)  & HipReplacement == T) | 
+               (is.finite(KneeReplacement) & KneeReplacement == T)]
+
+data.table::setkey(dt.tv.splits,patient_id,tstart,tstop)
+max.grp.col_(dt = 'dt.tv.splits',
+             max.var.name = 'sub.op',
+             aggregate.cols = 'sub.op',
+             id.vars = c("patient_id","end.fu"))
+n.type.events <- 1:2 #sort(unique(dt.tv[(postop.covid.cohort) ,event]))[-1]
+
+data.table::setkey(dt.tv.splits,patient_id,tstart,tstop)
+
+dt.tv.splits[event == 3, event := 2]
+data.table::setkey(dt.tv.splits, patient_id, end.fu, start)
+post.op.covid.model.split.sub <- 
+  lapply(n.type.events, function(i) survival::coxph(survival::Surv(start,end,event==i) ~  Colectomy + Cholecystectomy +  
+                                                      KneeReplacement + age.cat + 
+  sex + bmi.cat + imd5 + wave + vaccination.status.factor + region + Current.Cancer + Emergency*week.post.op +  Charl12 + recentCOVID + previousCOVID,
+   id = patient_id,
+  data = dt.tv.splits[(postop.covid.cohort) & start <=end & sub.op == T], model = T))
+
+newdata.rows <- 35 #
+
+newdata.pred <- data.table::data.table('start'  = 0:34,
+                                       'end' = 1:35,
+                                       'event' = rep(F,newdata.rows),
+                                      'week.post.op' = paste(rep(1:5,each = 7)),
+                                      'patient_id' = 1:newdata.rows,
+                                      'Colectomy' =  rep(T,newdata.rows),
+                                      'Cholecystectomy'= rep(F,newdata.rows),
+                                      'HipReplacement'= rep(F,newdata.rows),
+                                      'KneeReplacement'= rep(F,newdata.rows),
+                                      'age.cat' = rep('(50,70]',newdata.rows),
+                                      'sex' = rep('F',newdata.rows),
+                                      'bmi.cat' = rep(levels(dt.tv.splits$bmi.cat)[2],newdata.rows),
+                                      'imd5' = rep(levels(dt.tv.splits$imd5)[3], newdata.rows),
+                                      'wave' = rep(paste0('Wave_',3),times = newdata.rows),
+                                      'vaccination.status.factor' = rep('3',newdata.rows),
+                                      'region' = rep("East Midlands",newdata.rows),
+                                      'Current.Cancer' = rep(T,newdata.rows),
+                                      'Emergency' =  rep(F,newdata.rows),
+                                      'LOS.bin' =  rep(F,newdata.rows),
+                                      'Charl12' =  rep('Single',newdata.rows),
+                                      'recentCOVID' = rep(F,newdata.rows),
+                                      'previousCOVID' = rep(F,newdata.rows)
+                                      )
+#newdata.pred[,(procedures) := lapply(procedures, function(x) x == procedures[which.max(dt.tv[,lapply(.SD,sum,na.rm = T), .SDcols = c(procedures)])])]
+
+# newdata.pred[,(covariates[-c(1:length(procedures))]) := lapply(((length(procedures)+1):length(covariates)), function(i.c) {
+#   if(is.factor(dt.tv[!is.na(get(covariates[i.c])),get(covariates[i.c])])) {
+#     as.character(rep(max.category(i.c),newdata.rows))
+#   } else if(is.logical(dt.tv[!is.na(get(covariates[i.c])),get(covariates[i.c])])) {
+#     as.logical(rep(max.category(i.c),newdata.rows))
+#   } else if(is.numeric(dt.tv[!is.na(get(covariates[i.c])),get(covariates[i.c])])) {
+#     is.numeric(rep(max.category(i.c),newdata.rows))
+#   } else {
+#     rep(max.category(i.c),newdata.rows)
+#   }
+# })]
+
+base.haz <- lapply(n.type.events, function(i) survival::basehaz(post.op.covid.model.split.sub[[i]],centered = F))
+
+base.haz.comp <- lapply(n.type.events, function(i) { data.table::data.table('time' = base.haz[[i]]$time,
+                                                                            base.haz = base.haz[[i]][,1] - 
+                                                                              c(0,head(base.haz[[i]][,1],-1)))})
+
+lp <- lapply(n.type.events, function(i) {  data.table::data.table('time' = 0:34,
+                                                                    'risk' = exp(predict(object = post.op.covid.model.split.sub[[i]],
+                                                                                         type = 'lp', 
+                                                                                         newdata = newdata.pred))) })
+
+base.haz.merge <- Reduce(x =base.haz.comp,f = function(x,y) merge(x,y,by = 'time', no.dups = T, suffixes = c(".x",".y"), all = T, sort = T))
+
+for (j in 1:ncol(base.haz.merge)) set(base.haz.merge, which(!is.finite(base.haz.merge[[j]])), j, 0)
+
+daily.post.op.risk.sub <- 
+  unlist(round(100*apply(exp(apply(safelog(1 - Reduce('+',lapply(n.type.events, function(i) {
+    lp[[i]][base.haz.merge[order(time),.SD,.SDcols = c(1,i+1)],,roll =Inf,on = 'time', rollends = c(T,T)][time >= 0][order(time),.(.SD[,1]*.SD[,2]),.SDcols = c(2:3)] 
+  }))),2,cumsum))*
+    lp[[1]][base.haz.merge[order(time),.SD,.SDcols = c(1,2)],,roll =Inf,on = 'time', rollends = c(T,T)][time >= 0][order(time),.(.SD[,1]*.SD[,2]),.SDcols = c(2:3)] ,2,cumsum), digits = 3))
+
+daily.post.op.risk.sub[!is.finite(daily.post.op.risk.sub)] <- 0
+
+times.comb <- unique(sort(unlist(base.haz.merge$time)))[unique(sort(unlist(base.haz.merge$time))) >= 0]
+
+daily.post.op.risk.sub  <-  data.table::data.table(daily.post.op.risk.sub - c(0,daily.post.op.risk.sub[-length(daily.post.op.risk.sub)]),
+                                                `Days post op` = times.comb)[times.comb <=35,]
+
+data.table::fwrite(daily.post.op.risk.sub, file = here::here("output","daily_postopcovid_tv_sub.csv"))
+
+ggplot2::ggplot(daily.post.op.risk.sub) +
+  ggplot2::geom_line(ggplot2::aes(x = `Days post op`, y = risk)) + 
+  ggplot2::ylab("Daily risk (%)") +
+  ggplot2::theme_minimal() +
+  ggplot2::ggtitle("Daily risk of COVID from day of operation",
+                   subtitle = "Predicted for abdominal elective cancer operation in 50-70 year old female\n BMI 20-25, 3rd IMD quintile with single morbidity\n booster vaccination and no previous COVD")
+ggplot2::ggsave( filename = here::here("output","dailyCovidRiskSub.pdf"),device = "pdf",width = 8, height = 8, units = 'in',dpi = 'retina')
+
+##############
+
+dt.tv.splits[, `:=`(start = tstart - los.end,
+                    end = tstop - los.end)]
+dt.tv.splits <- dt.tv.splits[is.finite(los.end) & start>0 ] # Need to start follow up day after discharge to avoid discharge diagnoses
+
+
+post.op.VTE.model.split.sub <- 
+  lapply(n.type.events, function(i) survival::coxph(survival::Surv(start,end,event.VTE==i) ~  Colectomy + survival::strata(postcovid) +    
+                                                      Cholecystectomy + KneeReplacement + age.cat + 
+                                            sex + bmi.cat + imd5 + wave + vaccination.status.factor + region + 
+                                            Current.Cancer + Emergency*week.post.disch + LOS.bin + Charl12 + recentCOVID + previousCOVID, 
+                                          id = patient_id,
+                                          data = dt.tv.splits[(postcovid.VTE.cohort) & start <=end], model = T))
+
+newdata.rows = 35
+
+newdata.pred <- data.table::data.table('start' = rep(0:34, times = 2),
+                                       'end' = rep(1:35,times = 2),
+                                       'event.VTE' = rep(F,newdata.rows*2),
+                                       'week.post.disch' = paste(rep(ceiling(1:35 / 7), times = 2)),
+                                       'patient_id' = rep(1:2,each = newdata.rows),
+                                       'Colectomy' =  rep(T,newdata.rows*2),
+                                       'Cholecystectomy'= rep(F,newdata.rows*2),
+                                       'HipReplacement'= rep(F,newdata.rows*2),
+                                       'KneeReplacement'= rep(F,newdata.rows*2),
+                                       'postcovid'=rep(c(0,1), each = newdata.rows),
+                                       'Vascular'=rep(F,newdata.rows*2),
+                                       'age.cat' = rep('(50,70]',newdata.rows*2),
+                                       'sex' = rep('F',newdata.rows*2),
+                                       'bmi.cat' = rep(levels(dt.tv.splits$bmi.cat)[2],newdata.rows*2),
+                                       'imd5' = rep(levels(dt.tv.splits$imd5)[3], newdata.rows*2),
+                                       'wave' = rep(paste0('Wave_',3),times = newdata.rows*2),
+                                       'vaccination.status.factor' = rep('3',newdata.rows*2),
+                                       'region' = rep("East Midlands",newdata.rows*2),
+                                       'Current.Cancer' = rep(T,newdata.rows*2),
+                                       'Emergency' =  rep(F,newdata.rows*2),
+                                       'LOS.bin' =  rep(F,newdata.rows*2),
+                                       'Charl12' =  rep('Single',newdata.rows*2),
+                                       'recentCOVID' = rep(F,newdata.rows*2),
+                                       'previousCOVID' = rep(F,newdata.rows*2)
+)
+
+base.haz <- lapply(n.type.events, function(i) survival::basehaz(post.op.VTE.model.split.sub[[i]],centered = F))
+
+base.haz.comp <- lapply(n.type.events, function(i) { data.table::data.table('time' = base.haz[[i]]$time,
+                                                                            base.haz = base.haz[[i]][,1] - 
+                                                                              c(0,head(base.haz[[i]][,1],-1)))})
+
+lp <- lapply(n.type.events, function(i) {  data.table::dcast(fun.aggregate = mean,
+  data.table::data.table('patient_id' = rep(1:2, each = 5),
+  'time' = rep(0:89,each = 2),
+                                                                    'risk' = exp(predict(object = post.op.VTE.model.split.sub[[i]],
+                                                                                         type = 'lp', 
+                                                                                         newdata = newdata.pred))),time ~patient_id, value.var = 'risk')})
+
+base.haz.merge <- Reduce(x =base.haz.comp,f = function(x,y) merge(x,y,by = 'time', no.dups = T, suffixes = c(".x",".y"), all = T, sort = T))
+
+for (j in 1:ncol(base.haz.merge)) set(base.haz.merge, which(!is.finite(base.haz.merge[,(j)])), j, 0)
+
+daily.post.op.VTE.risk.sub <- 
+  unlist(round(100*apply(exp(apply(safelog(1 - Reduce('+',lapply(n.type.events, function(i) {
+    lp[[i]][base.haz.merge[order(time),.SD,.SDcols = c(1,i+1)],,roll =Inf,on = 'time', rollends = c(T,T)][time >= 0][order(time),.(.SD[,1]*.SD[,3], .SD[,2]*.SD[,3]),.SDcols = c(2:4)] 
+  }))),2,cumsum))*
+    lp[[1]][base.haz.merge[order(time),.SD,.SDcols = c(1,2)],,roll =Inf,on = 'time', rollends = c(T,T)][time >= 0][order(time),.(.SD[,1]*.SD[,3], .SD[,2]*.SD[,3]),.SDcols = c(2:4)], 2, cumsum ), digits = 3))
+
+daily.post.op.VTE.risk.sub[!is.finite(daily.post.op.VTE.risk.sub)] <- 0
+
+times.comb <- unique(sort(unlist(base.haz.merge$time)))[unique(sort(unlist(base.haz.merge$time))) >= 0]
+
+daily.post.op.VTE.risk.sub <- as.vector(daily.post.op.VTE.risk.sub - rbind(c(0,0),daily.post.op.VTE.risk.sub[-nrow(daily.post.op.VTE.risk.sub),]))
+daily.post.op.VTE.risk.sub  <-  cbind(data.table::data.table("COVID"= rep(c(F,T), each = length(times.comb)),
+"Risk" = daily.post.op.VTE.risk.sub,
+"Days post discharge" = rep(times.comb, times = 2)))
+######################
+ggplot2::ggplot(daily.post.op.VTE.risk.sub) +
+  ggplot2::geom_line(ggplot2::aes(x = `Days post discharge`, y = Risk, colour = COVID)) + 
+  ggplot2::ylab("Daily risk (%)") +
+  ggplot2::theme_minimal() +
+  ggplot2::ggtitle("Daily risk of VTE from day of discharge", subtitle = "Predicted for abdominal elective cancer operation in 50-70 year old female\n BMI 20-25, 3rd IMD quintile with single morbidity\n booster vaccination and no previous COVD")
+ggplot2::ggsave( filename = here::here("output","dailyVTERiskSub.pdf"),device = "pdf",width = 8, height = 8, units = 'in',dpi = 'retina')
+
+
+##################################
+save(daily.post.op.risk.sub,daily.post.op.VTE.risk.sub, file = here::here("output","postopcovid_tv_daily_sub.RData"))
+data.table::fwrite(daily.post.op.VTE.risk.sub, file = here::here("output","daily_postopcovid_VTE_tv_sub.csv"))
+
+
