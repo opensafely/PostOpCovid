782aa2d118b5817699b4bfc53ac114d7085937e9
ColinCrooks <colin.j.crooks@gmail.com>
2023-01-07 23:19:30 +0000

    Update data_manage.R

--- analysis/data_manage.R
+++ analysis/data_manage.R
@@ -40,11 +40,12 @@ rm(dt.update)
 dt.major <- Reduce(function(...) {
   merge(..., by = c('patient_id'), all = T)
 },lapply(procs, 
-         function(x) trycatch(data.table::fread(here::here('output',
+         function(x) tryCatch(data.table::fread(here::here('output',
                                                   paste0('input_',x,'_majorminor.csv')))[,
                                                   c('date_admitted','date_discharged') := NULL], 
-                                                  error = function(e) dt[,.(patient_id,
-                                                  paste0('input_',x,'_majorminor.csv') = NA)])
+                                                  error = function(e) EVAL(paste0("dt[,.('patient_id' = patient_id,
+                      '",
+                                                                                  x,"_Major_HES_binary_flag'  = NA)]")))
                                                   ))
 
 data.table::setkey(dt,patient_id)
