5fabbbe2e659ff7fcbd6f9bd133d37e05d02e692
andrewjackson4 <111368677+andrewjackson4@users.noreply.github.com>
2023-10-27 15:41:53 +0100

    cumulative incidence by procedure across covid waves

---
+++ analysis/table_30day_cumulative_incidence_waves.R
@@ -0,0 +1,71 @@
+# Load libraries
+
+library(data.table)  
+library(survival)   
+
+# Load data
+dt.tv <- data.table::setDT(arrow::read_feather(here::here("output", "cohort_long.feather")))
+
+#############################################
+
+# Define procedures and waves
+procedures <- c("Abdominal", "Obstetrics", "Orthopaedic", "CardioThoracicVascular")
+waves <- c("Wave_1", "Wave_2", "Wave_3", "Wave_4")
+
+# Define a function to calculate outcomes for each procedure and wave
+
+cuminc.km.vte_wave <- function(proc, wv) {
+  safelog <- function(x) {
+    x[x < 1e-200] <- 1e-200
+    log(x)
+  }
+  data.table::setkey(dt.tv, patient_id, tstart, tstop)
+  
+  # Filter the dataset for the current procedure and wave
+  filtered_data <- dt.tv[get(proc) == 1 & wave == wv & start >= 0 & (tstop - tstart) <= 30 & any.op.VTE == TRUE]
+  
+  # Check if there are observations in the filtered data
+  if (nrow(filtered_data) > 0) {
+    
+    # Perform survival analysis to calculate outcomes
+    surv_fit <- survival::survfit(survival::Surv(start, end, event.VTE == 1) ~ 1, data = filtered_data, id = patient_id)
+    n_risk <- summary(surv_fit, times = 0)$n.risk
+    n_event <- summary(surv_fit, times = 30)$n.event
+    surv_30_day <- 1 - summary(surv_fit, times = 30)$surv
+    cuminc_30_day_incidence <- 100 * round(surv_30_day, digits = 4)
+  } else {
+    # If no data, set outcomes to 0
+    n_risk <- 0
+    n_event <- 0
+    cuminc_30_day_incidence <- 0
+  }
+  
+  # Return the calculated outcomes
+  return(data.table(Procedure = proc, Wave = wv, num_at_risk = n_risk, num_events = n_event, cuminc_30_day = cuminc_30_day_incidence))
+}
+
+# Create an empty results table to store the outcomes
+procedure_across_waves <- data.table(Procedure = character(),
+                           Wave = character(),
+                           num_at_risk = numeric(),
+                           num_events = numeric(),
+                           cuminc_30_day = numeric())
+  
+# Calculate outcomes for each combination of procedure and wave
+for (proc in procedures) {
+  for (wv in waves) {
+    procedure_across_waves <- rbind(procedure_across_waves, cuminc.km.vte_wave(proc, wv))
+  }
+}
+
+# Check output in console
+print(procedure_across_waves)
+
+# Name the columns of the results table
+names(procedure_across_waves) <- c("Procedure", "Wave", "Number at risk", "Number of post operative VTE events within 30 days", 
+                    "30 day Cumulative VTE")
+
+# Save the results to CSV and RData
+data.table::fwrite(procedure_across_waves, file = here::here("output", "table_postop_VTEincidence_by_procedure_wave.csv"))
+save(procedure_across_waves, file = here::here("output", "table_postop_VTEincidence_by_procedure_wave.RData"))
+
