847ef73f7d0e6f2180314ed5da5ac49a81a602b8
andrewjackson4 <111368677+andrewjackson4@users.noreply.github.com>
2023-10-31 15:54:09 +0000

    update to any.op==T

--- analysis/Utils.R
+++ analysis/Utils.R
@@ -681,13 +681,12 @@ cuminc.km.mort.sub <- function(x,niter)  {
 
 
 
-
-## new cumulative incidence for 30 day VTE
+## new cumulative incidence for 30 day VTE- any.op.VTE==1 replaced with any.op==T
 cuminc.km.vte <- function(x, niter)  { 
   safelog <- function(x) { x[x < 1e-200] <- 1e-200; log(x) }
   data.table::setkey(dt.tv, patient_id, tstart, tstop)
   
-  filtered_data <- dt.tv[start >= 0 & (tstop - tstart) <= 30 & any.op.VTE == T & !is.na(get(x))]
+  filtered_data <- dt.tv[start >= 0 & (tstop - tstart) <= 30 & any.op == T & !is.na(get(x))]
   
   return(cbind(rep(x, length(levels(filtered_data[, as.factor(get(x))]))),
                rnd(data.table::setDT(summary(survival::survfit(survival::Surv(start, end, event.VTE == 1) ~ get(x), 
@@ -718,6 +717,8 @@ cuminc.km.vte <- function(x, niter)  {
 #                                                                           data = dt.tv[tstart >=0 & any.op.VTE == T & !is.na(get(x)),],
 #                                                                           id = patient_id), times = 30)[c('surv')])), digits = 4)))
 # }
+
+
 
 
 
